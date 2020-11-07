13aa5fa6ba8660147fe32aa30a33b4865668dbc8
Antonio Alvarez <tokyo.blue.tsubasa@gmail.com>
2020-11-07 15:19:18 -0600

    Comida Sana

---
+++ app/src/main/java/com/example/comidasana/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.comidasana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),FirstPage.class));
+            }
+        });
+    }
+}
